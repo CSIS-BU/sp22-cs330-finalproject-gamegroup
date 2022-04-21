b99f13ec916148e305492f14a415edd1e08af8bb
ekirchner01 <89597163+ekirchner01@users.noreply.github.com>
2022-04-20 21:54:49 -0500

    Update logictest.py

--- logictest.py
+++ logictest.py
@@ -1,40 +1,91 @@
 import random
 from colorama import init, Back, Fore
+import sys 
+import socket 
 init()
 
 wordList = ["Pilot", "Pound", "Queen", "Ratio", "Shock", "Sheep", "Skill"]
 word = random.choice(wordList).lower()
 
+SEND_BUFFER_SIZE = 2048 
+ 
+def client(server_ip, server_port): 
+    """TODO: Open socket and send message from sys.stdin""" 
+    # create an INET, STREAMing socket 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+        # now connect to server 
+        s.connect((server_ip, server_port)) 
+         
+        count = 0
+        
+        while count < 6: 
+            content = sys.stdin.buffer.read(SEND_BUFFER_SIZE) 
+            if not content: break 
+            sent = s.sendall(content) 
+            if sent == 0: 
+                raise RuntimeError("socket connection broken")
+              
+            count++
+            
+            data = clientsocket.recv(RECV_BUFFER_SIZE) 
+              
+            
+    pass 
+
 def main():
-    #This is for testing purposes, just prints out the chosen word.
-    print("The server chose: " + word)
+    
+    """Parse command-line arguments and call client function """ 
+    if len(sys.argv) != 2: 
+        sys.exit("Usage: python3 wordle.py [Server IP] [Server Port]") 
+    server_ip = sys.argv[1] 
+    server_port = int(sys.argv[2]) 
+    client(server_ip, server_port) 
+    
+    """TODO: Open socket and send message from sys.stdin""" 
+    # create an INET, STREAMing socket 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+    
+        # now connect to server 
+        s.connect((server_ip, server_port)) 
+    
+        word = clientsocket.recv(RECV_BUFFER_SIZE) 
+        if not data: break
+            
+        #This is for testing purposes, just prints out the chosen word.
+        print("The server chose: " + word)
 
-    # Inform player of general rules
-    print("You've been randomly assigned a 5 letter word. \n"
-          "You have 5 attempts to guess the correct word.")
+        # Inform player of general rules
+        print("You've been randomly assigned a 5 letter word. \n"
+              "You have 5 attempts to guess the correct word.")
 
-    # The player has 5 attempts to guess
-    for attempts in range(1, 5):
-        print("Guess #" + str(attempts) + ": ")
+        # The player has 5 attempts to guess
+        for attempts in range(1, 5):
+            print("Guess #" + str(attempts) + ": ")
 
-        guess = input()
+            guess = sys.stdin.buffer.read(SEND_BUFFER_SIZE) 
 
-        response = ""
-        for i in range(len(word)):
-            #Compare each letter of the guessed word to the correct word and change it's color, print response to the player.
-            if guess[i] == word[i]:
-                #Green and yellow wasn't really working in my terminal so I'm using these for now
-                response += Back.CYAN + Fore.BLACK + guess[i] + Fore.RESET + Back.RESET
-            elif guess[i] in word:
-                response += Back.YELLOW + Fore.BLACK + guess[i] + Fore.RESET + Back.RESET
-            else:
-                response += guess[i] + Back.RESET
-        print(response)
+            response = ""
+            for i in range(len(word)):
+                #Compare each letter of the guessed word to the correct word and change it's color, print response to the player.
+                if guess[i] == word[i]:
+                    #Green and yellow wasn't really working in my terminal so I'm using these for now
+                    response += Back.CYAN + Fore.BLACK + guess[i] + Fore.RESET + Back.RESET
+                elif guess[i] in word:
+                    response += Back.YELLOW + Fore.BLACK + guess[i] + Fore.RESET + Back.RESET
+                else:
+                    response += guess[i] + Back.RESET
+            print(response)
 
-        #If the user guesses the word correctly then the game is over
-        if word == guess:
-            print("You guess correctly!!")
-            break;
+            sent = s.sendall(guess) 
+            if sent == 0: 
+                raise RuntimeError("socket connection broken")
+            
+            flag = clientsocket.recv(RECV_BUFFER_SIZE) 
+            
+            #If the user guesses the word correctly then the game is over
+            if flag == "YES":
+                print("You guess correctly!!")
+                break;
 
 if __name__ == "__main__":
     main()
